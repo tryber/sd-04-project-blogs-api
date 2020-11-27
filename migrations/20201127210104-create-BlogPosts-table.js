fd5d01693264ee3f6d25f14b8a9993eebfa6532f
Joao Soutto <joaosoutto@hotmail.com>
2020-11-27 18:02:40 -0300

    config inicial

---
+++ migrations/20201127210104-create-BlogPosts-table.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const BlogPostsTable = queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: { model: 'Users', key: 'id' },
+      },
+      published: {
+        allowNull: false,
+        unique: false,
+        type: Sequelize.STRING,
+      },
+      updated: {
+        allowNull: false,
+        unique: false,
+        type: Sequelize.STRING,
+      },
+    });
+    return BlogPostsTable;
+  },
+  down: async (queryInterface) => queryInterface.dropTable('BlogPosts'),
+};
