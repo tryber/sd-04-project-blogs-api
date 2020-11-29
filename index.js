e1bcdd4045a39237f7ced081a29cb6493ca1c602
Filipe de Castro <filipeyoga@gmail.com>
2020-11-29 09:11:30 -0300

    Feat: Users

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express');
-
+require("dotenv").config();
+const express = require("express");
+const usersRoute = require("./routes/usersRoute");
+const loginRoute = require("./routes/loginRoute");
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
-
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
+app.get("/", (request, response) => {
   response.send();
 });
+
+// routes
+app.use(express.json());
+
+app.use("/user", usersRoute);
+app.use("/login", loginRoute);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
---
+++ routes/usersRoute.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const usersValidation = require("../middlewares/usersValidation");
+const { validateToken } = require("../services/auth");
+const router = express.Router();
+
+router.post(
+  "/",
+  usersValidation.checkName,
+  usersValidation.checkEmail,
+  usersValidation.checkPassword,
+  usersValidation.checkUserExists,
+  usersController.post
+);
+
+router.get("/", validateToken, usersController.get);
+
+router.get("/:id", validateToken, usersController.getById);
+
+router.delete("/me", validateToken, usersController.delete);
