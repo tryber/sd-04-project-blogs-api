3b4a0ab23231890a5687e52f4ea9d45f48081248
igorgbr <igorgphotoarte@gmail.com>
2020-11-29 20:39:18 -0300

    quarto de posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -54,4 +54,41 @@ router.get('/:id', validateJWT, async (req, res) => {
   res.status(200).json(posts[0]);
 });
 
+router.put('/:id', validateJWT, async (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title) {
+    res.status(400).json({ message: '"title" is required' });
+  }
+
+  if (!content) {
+    res.status(400).json({ message: '"content" is required' });
+  }
+
+  // Pesquisa o usuario logado
+  const user = await Users.findOne({ where: { displayName: req.user.data } });
+
+  // Pesquisa o Post chamado na URL
+  const oldPost = await Posts.findOne({
+    where: { id: req.params.id },
+    attributes: { exclude: ['published', 'updated'] },
+  });
+
+  // Compara userId do post com id do usuario logado
+  if (oldPost.userId !== user.id) {
+    res.status(401).json({ message: 'Usuário não autorizado' });
+  }
+
+  // Atualiza o post
+  await Posts.update({ title, content }, { where: { id: req.params.id } });
+
+  // Retorna o post atualizado
+  const newPost = await Posts.findOne({
+    where: { id: req.params.id },
+    attributes: { exclude: ['published', 'updated'] },
+  });
+
+  res.status(200).json(newPost);
+});
+
 module.exports = router;
