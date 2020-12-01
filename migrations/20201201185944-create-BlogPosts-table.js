a4432096674ca35c41d74e306064ba86378469b5
leandromarten1 <leandromarten1@gmail.com>
2020-12-01 16:08:38 -0300

    Migration BlogPosts

---
+++ migrations/20201201185944-create-BlogPosts-table.js
@@ -0,0 +1,38 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const blogPosts = await queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Users', key: 'id'},
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+    return blogPosts;
+  },
+
+  down: async (queryInterface, _Sequelize) => queryInterface.dropTable('BlogPosts'),
+};
