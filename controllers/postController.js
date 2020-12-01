a2234a4feb92400d4530f629f9b30784771a50df
leandromarten1 <leandromarten1@gmail.com>
2020-12-01 18:21:04 -0300

    Requisito 11

--- controllers/postController.js
+++ controllers/postController.js
@@ -59,8 +59,27 @@ router.put('/:id', validation.isPossibleInsertPost, auth, async (req, res) => {
       res.status(401).json({ message: 'Usuário não autorizado' });
     }
     await Post.update({ title, content }, { where: { id } });
-    const updatedPost = Post.findOne({ where: { id } });
+    const updatedPost = await Post.findOne({ where: { id } });
     return res.status(200).json(updatedPost);
+  } catch (err) {
+    return res.status(500).json({ message: 'Algo errado!', err });
+  }
+});
+
+router.delete('/:id', auth, async (req, res) => {
+  const { id } = req.params;
+  const { user } = req;
+  const post = await Post.findByPk(id);
+  if (!post) {
+    return res.status(404).json({ message: 'Post não existe' });
+  }
+  try {
+    if (post.userId !== user.id) {
+      return res.status(401).json({ message: 'Usuário não autorizado' });
+    }
+
+    await Post.destroy({ where: { id } });
+    return res.status(204).json();
   } catch (err) {
     return res.status(500).json({ message: 'Algo errado!', err });
   }
