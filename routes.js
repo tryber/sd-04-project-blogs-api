63de072b89db41b056eb355d04f66fed420b169f
orlando-messias <programadorlando@gmail.com>
2020-12-01 19:32:48 -0300

    endpoint put /post/:id

--- routes.js
+++ routes.js
@@ -14,5 +14,6 @@ routes.delete('/user/me', validateToken, userController.deleteUser);
 routes.post('/post', validateToken, postController.newPost);
 routes.get('/post', validateToken, postController.getPosts);
 routes.get('/post/:id', validateToken, postController.findPostById);
+routes.put('/post/:id', validateToken, postController.editPost);
 
 module.exports = routes;
