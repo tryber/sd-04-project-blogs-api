bb7965fcd6e5eee515a1149d53721bd5e16d8c3d
Josias Viveiro <josiasviveiro@gmail.com>
2020-12-01 17:19:31 -0300

    Iniciando o projeto Blogs API

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const app = express();
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
+/*
+Iniciando o projeto
+
+1. migration
+OK - 2. seed (Aparentemente veio pronto)
+3. models
+4. controllers
+*/
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
