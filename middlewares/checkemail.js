45d607c6f0557bd6a6807e3845b0967dc6bf39d2
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-12-01 23:19:08 -0300

    cc 02

--- middlewares/checkemail.js
+++ middlewares/checkemail.js
@@ -1,7 +1,6 @@
-const checkemail = async (req, res, next) => {
+const checkemail = async (req, res, next) =>
   req.body.email
     ? next()
     : res.status(400).json({ message: '"email" is required' });
-};
 
 module.exports = checkemail;
