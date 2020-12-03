6ab8eef07954fa1349e39973f8a31af0c101ef1d
lourenco <loulage@gmail.com>
2020-12-03 17:19:54 -0300

    index fix

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const bodyParse = require('body-parser');
 
-const userRouter = require('./routers/userRouters');
-const loginRouter = require('./routers/loginRouter');
-
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParse.json());
 
-app.use('/user', userRouter);
-app.use('/login', loginRouter);
+// app.use('/user', userRouter);
+// app.use('/login', loginRouter);
 
 app.get('/', (_req, res) => res.send());
 app.listen(PORT, () => console.log(`ouvindo na porta ${PORT}!`));
