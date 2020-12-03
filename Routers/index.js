b0f9c01a30b4355c561ca401f233eb0729c62a40
Tereza Cristina Carvalho <terezacristina@MacBook-Pro-de-Tereza.local>
2020-12-03 20:34:42 -0300

    req 6

--- Routers/index.js
+++ Routers/index.js
@@ -1,5 +1,5 @@
 const loginRouter = require('./loginRouter');
 const postsRouter = require('./postsRouter');
-const userRouter = require('./userRouter');
+const userRouter = require('./UserRouter');
 
 module.exports = { userRouter, loginRouter, postsRouter };
