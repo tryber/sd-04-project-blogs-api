86bba36af5af78d633f91bdc5fdc224be252d66c
RobertoAsgon <robertoasgon@gmail.com>
2020-12-03 23:35:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+// Iniciando...
+
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
