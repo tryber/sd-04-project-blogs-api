7925640e2fce1a8f0b47f6400b40fa4b729925a6
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-12-04 00:04:16 -0300

    cc

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,10 +4,10 @@ const { Post, User } = require('../models');
 
 const router = express.Router();
 // https://sequelize.org/v5/manual/querying.html
-router.get("/", validateJwt, async (req, res) => {
+router.get('/', validateJwt, async (req, res) => {
   const postsList = await Post.findAll({
-    attributes: { exclude: ["userId"] },
-    include: { model: User, as: "user" },
+    attributes: { exclude: ['userId'] },
+    include: { model: User, as: 'user' },
   });
   res.status(200).json(postsList);
 });
--- models/Post.js
+++ models/Post.js
@@ -1,39 +1,38 @@
 const createPost = (sequelize, DataTypes) => {
-    const Post = sequelize.define(
-      'Post',
-      {
-        title: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        content: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        userId: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          foreignKey: true,
-        },
-        published: {
-          type: DataTypes.DATE,
-          defaultValue: DataTypes.NOW
-        },
-        updated: {
-          type: DataTypes.DATE,
-          defaultValue: DataTypes.NOW
-        }
+  const Post = sequelize.define(
+    'Post',
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
-      { timestamps: false },
-    );
-    Post.associate = (models) => {
-        Post.belongsTo(models.User, {
-          foreignKey: 'userId',
-          as: 'user',
-        });
-      };
-    return Post;
+      content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        foreignKey: true,
+      },
+      published: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updated: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    { timestamps: false }
+  );
+  Post.associate = (models) => {
+    Post.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
   };
-  
-  module.exports = createPost;
-  
+  return Post;
+};
+
+module.exports = createPost;
