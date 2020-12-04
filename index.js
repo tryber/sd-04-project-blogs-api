288d4c55a37dd7fb07e36fc48fbb3e6a1e404c26
deboracosilveira <deboracosilveira@gmail.com>
2020-12-04 15:01:29 -0300

    order index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require('express');
 const usersControllers = require('./controllers/usersController');
 
 const app = express();
-app.use(express.json());
-
-// evaluator forever
-app.use('/user', usersControllers);
-
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use(express.json());
+
+app.use('/user', usersControllers);
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
