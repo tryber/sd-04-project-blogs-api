74873698735961163187d4d23d5241b09d71a499
lourenco <loulage@gmail.com>
2020-12-08 17:34:40 -0300

    getallUsers done

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { Users } = require('../models');
 const createToken = require('../services/createToken');
+const validateToken = require('../services/validateToken');
 const {
   displayNameValidation,
   isEmailValid,
@@ -13,17 +14,19 @@ const {
 const router = Router();
 
 // Listar todos usuários.
-router.get('/', async (_req, res) => {
-  try {
-    console.log('aqui getall');
-    const users = await Users.findAll({ attributes: { exclude: ['password'] } });
-    console.log(JSON.stringify(users));
-    res.status(200).json(users);
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ message: 'internal error' });
-  }
-});
+router.get('/',
+  validateToken,
+  async (_req, res) => {
+    try {
+      console.log('aqui getall');
+      const users = await Users.findAll({ attributes: { exclude: ['password'] } });
+      console.log(JSON.stringify(users));
+      res.status(200).json(users);
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ message: 'internal error' });
+    }
+  });
 
 // cadastrar um novo usuário.
 router.post('/',
