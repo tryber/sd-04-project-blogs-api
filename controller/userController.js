ac65ed181741e8cdd2c4730c80e23b399dc8b550
Thiago Tavares <thiagooo15@gmail.com>
2020-12-08 12:17:21 -0300

    what if

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { Users } = require('../models');
 const JWT = require('../service');
 const middlewares = require('../middlewares');
+const validateToken = require('../service/validateJWT');
 
 const router = express.Router();
 
@@ -22,3 +23,12 @@ router.get('/', JWT.validateJWT, async (req, res) => {
   const allUsers = await Users.findAll({ attributes: { exclues: ['password'] } });
   return res.status(200).json(allUsers);
 });
+
+router.get('/:id', JWT.validateJWT, async(req, res) => {
+  const { id } = req.params;
+  const user = await Users.findOne({ where: { id } });
+  if (!user) {
+    return res.status(404).json({ message: 'Usuário não existe' });
+  }
+  return res.status(200).json(user);
+};
