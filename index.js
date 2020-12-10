c8c85c4317d3d278906e2e89114bfa550d25c3a4
gabrielukke <gabriellukk@outlook.com>
2020-12-10 19:42:15 -0300

    Change PORT variable

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const { userRouter, loginRouter, postRouter } = require('./routes');
 
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
