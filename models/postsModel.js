5c9f694bca52148f0fb6c816aef75321d1a0bc12
ribaslucas <lucas@mundoribas.com.br>
2020-12-12 14:00:38 -0300

    Primieiro CC

---
+++ models/postsModel.js
@@ -0,0 +1,28 @@
+const createPost = (sequelize, DataTypes) => {
+    const Post = sequelize.define(
+      'Post',
+      {
+        title: DataTypes.STRING,
+        content: DataTypes.STRING,
+        userId: DataTypes.INTEGER,
+        published: {
+          type: DataTypes.DATE,
+          defaultValue: DataTypes.NOW,
+        },
+        updated: {
+          type: DataTypes.DATE,
+          defaultValue: DataTypes.NOW,
+          onUpdate: DataTypes.NOW,
+        },
+      },
+      { timestamps: false },
+    );
+  
+    Post.associate = (models) => {
+      Post.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
+    };
+  
+    return Post;
+  };
+  
+  module.exports = createPost;
