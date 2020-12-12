1e68cbb50b36d1fd9dc88223b4235305812ed0a0
ribaslucas <lucas@mundoribas.com.br>
2020-12-12 12:06:50 -0300

    Criando tabelas com sequelize

---
+++ migrations/20201212144046-create-users.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const UsersTable = queryInterface.createTable(
+      'Users',
+      {
+        id: {
+          alllowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        displayName: {
+          allowNull: true,
+          type: Sequelize.STRING,
+        },
+        email: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        password: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        image: {
+          allowNull: true,
+          type: Sequelize.STRING,
+        },
+      },
+      { timestamps: false },
+    );
+    return UsersTable;
+  },
+
+  down: async (queryInterface) => queryInterface.dropTable('Users'),
+};
