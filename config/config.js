740b4ece02cfee20d23b4450a3b74e7de8155e69
danilossousa <danilosantossousa@gmail.com>
2020-12-13 03:14:18 -0300

    init 4

--- config/config.js
+++ config/config.js
@@ -1,29 +1,5 @@
 require('dotenv/config');
 
-// {
-//   "development": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_development",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "test": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_test",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "production": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_production",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   }
-// }
-
 module.exports = {
   development: {
     username: process.env.MYSQL_USER,
