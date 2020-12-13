04c72593c833fa5429c4ec535850b1d52d013718
danilossousa <danilosantossousa@gmail.com>
2020-12-13 11:46:33 -0300

    v5.3.1

--- models/postsModel.js
+++ models/postsModel.js
@@ -1,21 +1,21 @@
-// const postsModel = (sequelize, DataTypes) => {
-//   const Posts = sequelize.define(
-//     'Posts',
-//     {
-//       title: DataTypes.STRING,
-//       content: DataTypes.STRING,
-//       userId: { type: DataTypes.INTEGER, foreignKey: true },
-//       published: DataTypes.DATE,
-//       updated: DataTypes.DATE,
-//     },
-//     { timestamps: false },
-//   );
+const postsModel = (sequelize, DataTypes) => {
+  const Posts = sequelize.define(
+    'Posts',
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: { type: DataTypes.INTEGER, foreignKey: true },
+      published: DataTypes.DATE,
+      updated: DataTypes.DATE,
+    },
+    { timestamps: false },
+  );
 
-//   Posts.associate = (models) => {
-//     Posts.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
-//   };
+  Posts.associate = (models) => {
+    Posts.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
+  };
 
-//   return Posts;
-// };
+  return Posts;
+};
 
-// module.exports = postsModel;
+module.exports = postsModel;
