43d823e7d1f6050a7e0d6979824691e84e595fae
Josias Viveiro <josiasviveiro@gmail.com>
2020-12-16 17:15:24 -0300

    validateToken - spaces

--- middlewares/validateToken.js
+++ middlewares/validateToken.js
@@ -4,13 +4,13 @@ const secret = require('./secret');
 const validateToken = (req, res, next) => {
   try {
     const token = req.headers.authorization;
-    if(!token) res.status(401).json({message: 'Token não encontrado'});
+    if (!token) res.status(401).json({ message: 'Token não encontrado' });
     const valitation = jwt.verify(token, secret);
     const { password: _, user } = valitation;
     req.user = user;
     next();
   } catch (error) {
-    res.status(401).json({message: 'Token expirado ou inválido'});
+    res.status(401).json({ message: 'Token expirado ou inválido' });
   }
 };
 
