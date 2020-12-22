cef2807fb3a16c8adf89e53574db04e625b941e2
Josias Viveiro <josiasviveiro@gmail.com>
2020-12-22 12:40:41 -0300

    Req. 7 - GET /post - lista todos os posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { Posts } = require('../models');
+const { Posts, Users } = require('../models');
 const { validateToken } = require('../middlewares/validateToken');
 
 const router = Router();
@@ -20,6 +20,23 @@ router.post('/', validateToken, async (req, res) => {
     }
     const newPost = await Posts.create({ title, content, userId: id, published, updated });
     return res.status(201).json(newPost);
+  } catch (_e) {
+    return res.status(500).json({ message: 'internal error' });
+  }
+});
+
+// Req. 7 - Lista todos os posts
+router.get('/', validateToken, async (req, res) => {
+  try {
+    const posts = await Posts.findAll({
+      include: {
+        model: Users,
+        as: 'user',
+        attributes: { exclude: ['password'] },
+      },
+      attributes: { exclude: ['userId'] },
+    });
+    return res.status(200).json(posts);
   } catch (_e) {
     return res.status(500).json({ message: 'internal error' });
   }
