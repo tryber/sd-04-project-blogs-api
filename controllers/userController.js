2c0e8e26c3403b056fdc453b319a5543443cb367
wistyk <leonardoxw@gmail.com>
2020-12-25 19:41:58 -0300

    rota user/id

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,4 +29,21 @@ users.get('/', tokenValidation, (_, res) => {
     .catch((error) => console.log(error));
 });
 
+users.get('/:id', tokenValidation, (req, res) => {
+  const { id } = req.params;
+  User.findOne({
+    where: { id },
+    attributes: {
+      exclude: ['password'],
+    },
+  })
+    .then((selectedUser) => {
+      if (selectedUser === null) {
+        return res.status(404).json({ message: 'Usuário não existe' });
+      }
+      res.status(200).json(selectedUser);
+    })
+    .catch((error) => console.log(error));
+});
+
 module.exports = users;
