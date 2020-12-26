545b2dd2a9e81a4960f9c4017c13c8256fdc724d
wistyk <leonardoxw@gmail.com>
2020-12-26 14:06:55 -0300

    retirado a const do update

--- controllers/postController.js
+++ controllers/postController.js
@@ -56,7 +56,7 @@ posts.get('/:id', tokenValidation, existingId, (req, res) => {
 posts.put('/:id', tokenValidation, userCheck, existingValues, async (req, res) => {
   const { id } = req.params;
   const { title, content } = req.body;
-  const result = await Post.update(
+  await Post.update(
     {
       title,
       content,
