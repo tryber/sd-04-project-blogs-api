912262bc6ee1abc27d909bae61b1c62c7cc4bb04
wistyk <leonardoxw@gmail.com>
2020-12-25 22:22:35 -0300

    rota createPost

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,15 +1,28 @@
 const { Router } = require('express');
 
-const { Post } = require('../models');
+const validateToken = require('../auth/validateToken');
+
+const { tokenValidation } = require('../middlewares/userValidations');
+
+const { existingValues } = require('../middlewares/postValidations');
+
+const { Post, User } = require('../models');
 
 const posts = Router();
 
-posts.post('/', (req, res) => {
-  const { title, content, userId } = req.body;
-
-  Post.create({ title, content, userId })
-    .then((newPost) => res.status(200).json(newPost))
-    .catch((error) => console.log(error));
+posts.post('/', tokenValidation, existingValues, (req, res) => {
+  const { title, content } = req.body;
+  const token = req.headers.authorization;
+  const { email } = validateToken(token);
+  User.findOne({
+    where: { email },
+  }).then((user) =>
+    Post.create({ title, content, userId: user.dataValues.id })
+      .then((newPost) => {
+        const { id, updated, published, ...all } = newPost.dataValues;
+        return res.status(201).json(all);
+      })
+      .catch((error) => console.log(error)));
 });
 
 module.exports = posts;
