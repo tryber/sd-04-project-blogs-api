e290f16916957bb1f758539f29eca278e3b42dc6
Luma Arruda <luma.oarruda@gmail.com>
2021-01-01 17:23:24 -0300

    validações

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+require('dotenv').config;
 const express = require('express');
+const UsersController = require('./controllers/usersController');
+const LoginController = require('./controllers/LoginController');
+const PostsController = require('./controllers/PostsController');
 
 const app = express();
-
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const PORT = process.env.PORT
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use(express.json());
+
+app.use('/user', UsersController);
+app.use('/login', loginController);
+app.use('/post', postsController);
+
+app.listen(PORT, () => console.log(`ouvindo porta ${ PORT }!`));
---
+++ middlewares/buildResponse.js
@@ -0,0 +1,6 @@
+const buildResponse = (message) => {
+  const resp = { message };
+  return resp;
+};
+
+module.exports = { buildResponse };
