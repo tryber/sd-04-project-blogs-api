c959a7a830c6a00dd2a41d28385f9870e1058734
djonata-dias <diasdjonata@gmail.com>
2021-01-04 18:02:58 -0300

    ajuste dos middleware user vallidation e name validation

--- services/userServices.js
+++ services/userServices.js
@@ -4,11 +4,9 @@ const { Users } = require('../models');
 const messageError = (res, status, message) => res.status(status).json({ message });
 
 const userValidation = async (req, res, next) => {
-  const { email } = req.body;
-  const user = Users.findOne({
-    where: email,
+  const user = await Users.findOne({
+    where: { email: req.body.email },
   });
-
   if (user) {
     return messageError(res, 409, 'Usuário já existe');
   }
@@ -17,8 +15,8 @@ const userValidation = async (req, res, next) => {
 
 const nameValidation = (req, res, next) => {
   const { displayName } = req.body;
-
-  if (displayName.length < 8) {
+  
+  if (!displayName || displayName.length < 8) {
     return messageError(res, 400, '\'displayName\' length must be at least 8 characters long');
   }
   return next();
