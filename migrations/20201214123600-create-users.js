1d1dea17296be66d1c92ddd595b669b7e6fae552
djonata-dias <diasdjonata@gmail.com>
2021-01-04 16:14:48 -0300

    criei a migrations de users e posts

---
+++ migrations/20201214123600-create-users.js
@@ -0,0 +1,19 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const usersTable = queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      displayName: Sequelize.STRING,
+      email: Sequelize.STRING,
+      password: Sequelize.STRING,
+      image: Sequelize.STRING
+    });
+    return usersTable;
+  },
+
+  down: async (queryInterface, Sequelize) => queryInterface.dropTable('Users')
+
+};
---
+++ migrations/20201214124325-create-posts.js
@@ -0,0 +1,20 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const postsTable = queryInterface.createTable('Posts', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      title: Sequelize.STRING,
+      content: Sequelize.STRING,
+      userId: Sequelize.INTEGER,
+      published: Sequelize.DATE,
+      updated: Sequelize.DATE,
+    });
+    return postsTable;
+  },
+
+  down: async (queryInterface, Sequelize) => queryInterface.dropTable('Posts')
+
+};
