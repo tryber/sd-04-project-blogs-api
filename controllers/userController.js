f132bb40f2c5c66a3c37e71c8f482dae295bae7f
djonata-dias <diasdjonata@gmail.com>
2021-01-04 18:01:27 -0300

    ajuste na rota post do user e implatação do get

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,13 +4,14 @@ const { userValidation, nameValidation, passValidation, emailValidation } = requ
 
 const router = express.Router();
 
-router.post('/', userValidation, nameValidation, passValidation, emailValidation, (req, res) => {
+router.post('/', nameValidation, passValidation, emailValidation, userValidation, (req, res) => {
   const { displayName, email, password, image } = req.body;
-  console.log(displayName, email, password, image);
-  Users.findAll()
-    .then((users) => {
-      res.status(200).json(users);
-    });
+
+  return Users.upsert({ displayName, email, password, image }).then(user => res.status(200).json(user));
+});
+
+router.get('/', (req, res) => {
+  return Users.findAll().then(users => res.status(200).json(users));
 });
 
 module.exports = router;
