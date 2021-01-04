cbfcaf9ef614717ee276be5e923d738223cc5e42
henrique <hbaetaleite@gmail.com>
2021-01-04 12:08:18 -0300

    add blocos try/catch. add await on getAllUsers

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,39 +4,55 @@ const userMiddleware = require('../middlewares/userMiddleware');
 
 const insertNewUser = async (req, res) => {
   const { displayName, email, password, image } = req.body;
+  console.log(email);
 
-  const emailAlreadyExists = await userMiddleware.emailExists(email);
+  try {
+    const emailAlreadyExists = await userMiddleware.emailExists(email);
 
-  if (emailAlreadyExists) return res.status(409).json({ message: 'Usuário já existe' });
+    if (emailAlreadyExists) return res.status(409).json({ message: 'Usuário já existe' });
 
-  const user = await Users.create({ displayName, email, password, image });
+    const user = await Users.create({ displayName, email, password, image });
 
-  // PROVISÓRIO, ZUERA...
-  const token = user.image;
+    // PROVISÓRIO, ZUERA...
+    const token = user.image;
 
-  res.status(201).json({ token });
+    res.status(201).json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: 'Something wrong... Create user' });
+  }
 };
 
 const login = async (req, res) => {
   const { email, password } = req.body;
 
-  const notValidLogin = await userMiddleware.validateLogin(email, password);
+  try {
+    const notValidLogin = await userMiddleware.validateLogin(email, password);
 
-  if (notValidLogin) return res.status(400).json({ message: notValidLogin.message });
+    if (notValidLogin) return res.status(400).json({ message: notValidLogin.message });
 
-  const userExists = await userMiddleware.userExists(email, password);
+    const userExists = await userMiddleware.userExists(email, password);
 
-  if (!userExists) return res.status(400).json({ message: 'Campos inválidos' });
+    if (!userExists) return res.status(400).json({ message: 'Campos inválidos' });
 
-  const token = '127sjnjnxjn9hx3ssas7';
+    const token = '127sjnjnxjn9hx3ssas7';
 
-  return res.status(200).json(token);
+    return res.status(200).json(token);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: 'Something wrong...' });
+  }
 };
 
 const getAllUsers = async (req, res) => {
-  const users = Users.findAll({ attributes: { exclude: ['password'] } });
+  try {
+    const users = await Users.findAll({ attributes: { exclude: ['password'] } });
 
-  return res.status(200).json(users);
+    return res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: 'Something wrong...' });
+  }
 };
 
 module.exports = { insertNewUser, getAllUsers, login };
