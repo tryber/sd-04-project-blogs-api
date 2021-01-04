8145dda38ca927569e24b903f7fc8779f10ac49d
djonata-dias <diasdjonata@gmail.com>
2021-01-04 18:00:59 -0300

    ajuste de identação

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const bodyParser = require('body-parser');
-
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const { userController } = require('./controllers');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
