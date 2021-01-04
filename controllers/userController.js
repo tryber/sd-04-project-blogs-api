364e77248db9480417e744b429252a3dc3f118fc
djonata-dias <diasdjonata@gmail.com>
2021-01-04 16:42:01 -0300

    ajuste de CC

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,14 +3,13 @@ const { Users } = require('../models');
 const { userValidation, nameValidation, passValidation, emailValidation } = require('../services/userServices');
 const router = express.Router();
 
-router.post('/', userValidation, nameValidation, passValidation, emailValidation, (req, res, next) => {
+router.post('/', userValidation, nameValidation, passValidation, emailValidation, (req, res) => {
   const { displayName, email, password, image } = req.body;
+  console.log(displayName, email, password, image);
   Users.findAll()
     .then((users) => {
-      res.status(200).json(users)
+      res.status(200).json(users);
     });
 });
-
-
 
 module.exports = router;
