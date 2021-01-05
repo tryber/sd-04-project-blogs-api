ccc3e2693f5b967ba369b4eb5c5a1bf9bf4cf229
djonata-dias <diasdjonata@gmail.com>
2021-01-04 23:24:05 -0300

    adicionei a rota de login

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { userController } = require('./controllers');
+const { userController, loginController } = require('./controllers');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
 app.use('/user', userController);
+app.use('/login', loginController);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
