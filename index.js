91b751571baf020cbfee1baf10b57ef2da83f1a7
jessica-tavares <jessicatavaraeseel@gmail.com>
2021-01-05 15:07:31 -0300

    mais 1

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const app = express();
 app.use(express.json());
 app.use('/user', controllers.UserControllers);
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
 // app.listen(process.env.PORT, () => console.log(`ouvindo porta ${process.env.PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
