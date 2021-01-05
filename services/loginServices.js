e9e4bc23b6149a3e7f08d68301175bae295d4e1e
djonata-dias <diasdjonata@gmail.com>
2021-01-04 23:26:36 -0300

    configurei o service de validação de login

---
+++ services/loginServices.js
@@ -0,0 +1,23 @@
+const { Users } = require('../models');
+const jwt = require('jsonwebtoken');
+
+const jwtConfig = {
+  expiresIn: '1h',
+  algorithm: 'HS256',
+};
+const secret = 'NinguemNuncaVaiDescobrirEsteTokenSecreto';
+
+const userLoginValidation = (req, res) => {
+  const { email, password } = req.body;
+  
+  Users.findOne({ where: { email } })
+    .then(({ dataValues }) => {
+      if (email !== dataValues.email || password !== dataValues.password) {
+        return res.status(400).json({ message: 'Campos invalidos' });
+      }
+      const token = jwt.sign({ data: dataValues }, secret, jwtConfig);
+      res.status(200).json({ token });
+    });
+};
+
+module.exports = { userLoginValidation };
