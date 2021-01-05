702e02eb231e7083b6f54fe9b90c0933b5b4cea4
jessica-tavares <jessicatavaraeseel@gmail.com>
2021-01-05 16:39:45 -0300

    v3

--- middlewares/authUsers.js
+++ middlewares/authUsers.js
@@ -2,11 +2,11 @@ const UsersModel = require('../models');
 
 const isValidUser = async (req, res, next) => {
   const { displayName, email, password } = req.body;
-  const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+  const emailRegex = /[A-Z0-9]{1,}@[A-Z0-9]{2,}\.[A-Z0-9]{2,}/i;
   if (displayName.length <= 8) return res.status(400).json({ message: '"displayName" length must be at least 8 characters' });
-  if (emailRegex.test(email)) return res.status(400).json({ message: '"email" must be a valid email' });
-  if (email) return res.status(400).json({ message: '"email" is required' });
-  if (password.length >= 6) return res.status(400).json({ message: '"password" length must be at least 6 characters' });
+  if (!emailRegex.test(email)) return res.status(400).json({ message: '"email" must be a valid email' });
+  if (!email) return res.status(400).json({ message: '"email" is required' });
+  if (password.length <= 6) return res.status(400).json({ message: '"password" length must be at least 6 characters' });
   if (password.length === 0) return res.status(400).json({ message: '"password" is required' });
   const exists = await UsersModel.findOne({ where: { email } });
   if (exists) return res.status(409).json({ message: 'Usuário já esixte' });
