94c239902f05b30603770930456c526d3caf683c
djonata-dias <diasdjonata@gmail.com>
2021-01-07 14:18:23 -0300

    criei uma função para criação de token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,29 +1,35 @@
 const jwt = require('jsonwebtoken');
-const { Users } = require('../models');
 
 const secret = 'NinguemNuncaVaiDescobrirEsteTokenSecreto';
 
+const createToken = (payload) => {
+  const options = {
+    algorithm: 'HS256',
+    expiresIn: '7d',
+  };
+  const token = jwt.sign(payload, secret, options);
+
+  return token;
+};
+
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization;
-
-  if (!token) {
-    return res.status(401).json({ message: 'Token expirado ou invalido' });
-  }
-
   try {
+    const token = req.headers.authorization;
+    console.log(token);
+    if (!token) {
+      return res.status(401).json({ message: 'Token expirado ou invalido' });
+    }
     const decode = jwt.verify(token, secret);
-    const user = Users.findOne({ email: decode.data.email });
-
+    const user = decode.data;
     if (!user) {
       return res.status(401).json({ message: 'Erro ao procurar usuario do token' });
     }
-
     req.user = user;
-
-    next();
+    return next();
   } catch (err) {
+    // console.error(err);
     res.status(401).json({ message: 'Erro: Seu token é inválido' });
   }
 };
 
-module.exports = authMiddleware;
+module.exports = { authMiddleware, createToken };
