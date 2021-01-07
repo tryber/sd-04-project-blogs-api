dd61a2bcbad8b9bec6f098c7924227a3cb0fa6aa
djonata-dias <diasdjonata@gmail.com>
2021-01-07 14:22:46 -0300

    ajuste do controller de login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { userLoginValidation, loginEmailValidation, loginPassValidation } = require('../services/loginServices');
+const { userLoginValidation, dataLoginValidation } = require('../services/loginServices');
+const { createToken } = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.post('/', loginEmailValidation, loginPassValidation, userLoginValidation);
+router.post('/', dataLoginValidation, userLoginValidation, (req, res) => {
+  const { email, password } = req.body;
+  const token = createToken({ email, password });
+  return res.status(200).json({ token });
+});
 
 module.exports = router;
