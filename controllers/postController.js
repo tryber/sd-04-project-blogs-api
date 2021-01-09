a8ec0ca87556a2be6424c0deeddbcba71f11e2da
Ronald Lima <ronaldazevedolima@gmail.com>
2021-01-09 18:48:12 -0300

    req9

--- controllers/postController.js
+++ controllers/postController.js
@@ -40,7 +40,6 @@ postRoute.get('/', validatejwt, async (req, res) => {
 
 postRoute.get('/:id', validatejwt, async (req, res) => {
   try {
-    console.log('teste1');
     const post = await Posts.findByPk(req.params.id,
       { include: [{
         model: Users,
@@ -57,4 +56,29 @@ postRoute.get('/:id', validatejwt, async (req, res) => {
     return res.status(400).json({ message: 'algo deu errado' });
   }
 });
+
+postRoute.put('/:id', validatejwt, async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const updated = new Date();
+    if (!title || !content) {
+      const msg = title ? '"content" is required' : '"title" is required';
+      return res.status(400).json({ message: msg });
+    }
+    const updatepost = await Posts.update({ title, content, updated },
+      { where: { id: req.params.id, userId: req.user.id } });
+
+    if (updatepost[0] === 0) return res.status(401).json({ message: 'Usuário não autorizado' });
+
+    const post = await Posts.findOne({
+      where: { id: req.params.id },
+      attributes: { exclude: ['published', 'updated'] },
+    });
+
+    return res.status(200).json(post);
+  } catch (error) {
+    return res.status(400).json({ message: 'algo deu errado' });
+  }
+});
+
 module.exports = postRoute;
