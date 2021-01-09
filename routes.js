670faea9ff6665663e89fe9752da6adef2382129
Diego <diegodiko05@gmail.com>
2021-01-09 10:47:34 -0300

    Req 3

--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const userController = require('./controllers/userController');
+const { validateToken } = require('./middlewares/auth');
 
 const routes = Router();
 
 routes.post('/user', userController.newUser);
 routes.post('/login', userController.login);
+routes.get('/user', validateToken, userController.getUsers);
 
 module.exports = routes;
