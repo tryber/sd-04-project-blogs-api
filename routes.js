cde041499f20ac585c66c955de1cbce73308ed52
Diego <diegodiko05@gmail.com>
2021-01-09 11:24:26 -0300

    Req 8

--- routes.js
+++ routes.js
@@ -12,5 +12,6 @@ routes.get('/user/:id', validateToken, userController.findUserById);
 routes.delete('/user/me', validateToken, userController.deleteUser);
 routes.post('/post', validateToken, postController.newPost);
 routes.get('/post', validateToken, postController.getPosts);
+routes.get('/post/:id', validateToken, postController.findPostById);
 
 module.exports = routes;
