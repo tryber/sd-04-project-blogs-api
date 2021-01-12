f3c10b30199e69f901d2dd2cd95696f71c95a040
Thiago Tavares <thiagooo15@gmail.com>
2021-01-12 19:16:40 -0300

    reset

--- controller/index.js
+++
@@ -1,9 +0,0 @@
-const userController = require('./userController');
-// const loginController = require('./loginController');
-// const postController = require('./postController');
-
-module.exports = {
-  userController,
-  // loginController,
-  // postController,
-};
--- middlewares/index.js
+++
@@ -1,9 +0,0 @@
-const validadeUsers = require('./validadeUsers');
-// const validadeLogin = require("./validadeLogin");
-// const validadePosts = require("./validadePosts");
-
-module.exports = {
-  validadeUsers,
-  // validadeLogin,
-  // validadePosts,
-};
