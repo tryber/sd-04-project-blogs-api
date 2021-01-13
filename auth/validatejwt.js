952e80120bfdf28dd5e69cb574525d6de69d89bd
Alexandre Anicio <alexandre.anicio94@gmail.com>
2021-01-12 23:20:24 -0300

    try

--- auth/validatejwt.js
+++ auth/validatejwt.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const secret = process.env.SECRET;
 
 const validateJWT = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json({ message: 'Token não encontrado' });
   try {
-    const token = req.headers.authorization;
-    if (!token) return res.status(401).json({ message: 'Token não encontrado' });
     const validation = JWT.verify(token, secret);
     const { password: _, ...user } = validation;
     req.user = user;
