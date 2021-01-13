23d8bc7b904be105d9e2c5d5ce565672aa474fd7
Alexandre Anicio <alexandre.anicio94@gmail.com>
2021-01-12 23:02:46 -0300

    fix midd

--- auth/validatejwt.js
+++ auth/validatejwt.js
@@ -6,7 +6,7 @@ const secret = process.env.SECRET;
 const validateJWT = (req, res, next) => {
   try {
     const token = req.headers.authorization;
-    if (!token) res.status(401).json({ message: 'Token não encontrado' });
+    if (!token) return res.status(401).json({ message: 'Token não encontrado' });
     const validation = JWT.verify(token, secret);
     const { password: _, ...user } = validation;
     req.user = user;
