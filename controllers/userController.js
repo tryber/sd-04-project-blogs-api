2cdd8e7ed2d381a9e62590e837cd41aab87998c8
Alexandre Anicio <alexandre.anicio94@gmail.com>
2021-01-12 22:30:04 -0300

    req4

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,18 @@ const router = express.Router();
 router.get('/', validateJWT, async (req, res) => {
   try {
     const users = await User.findAll();
-    res.status(200).json({ users });
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get('/:id', validateJWT, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).json({ message: 'Usuário não existe' });
+    res.status(200).json(user);
   } catch (error) {
     console.error(error);
   }
