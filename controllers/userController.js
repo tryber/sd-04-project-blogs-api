ca10faf90515af0245607ffa9c1afd2c5bdc2d6e
Alexandre Anicio <alexandre.anicio94@gmail.com>
2021-01-12 22:21:17 -0300

    req3

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,18 @@ const express = require('express');
 const { User } = require('../models');
 const createToken = require('../auth/jwtCreate');
 const validator = require('../auth/validator');
+const validateJWT = require('../auth/validatejwt');
 
 const router = express.Router();
+
+router.get('/', validateJWT, async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json({ users });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 router.post('/', validator, async (req, res) => {
   try {
@@ -12,15 +22,18 @@ router.post('/', validator, async (req, res) => {
       where: { email: req.body.email },
     });
     if (!userExists) {
-      console.log(req.body, 'if');
-      const newUser = await User.create({ displayName, email, password, image });
+      const newUser = await User.create({
+        displayName,
+        email,
+        password,
+        image,
+      });
       const token = createToken(newUser.dataValues);
       res.status(201).json({ token });
     } else if (userExists) {
       res.status(409).json({ message: 'Usuário já existe' });
     }
   } catch (error) {
-    console.log(req.body, 'catch');
     const { message } = error;
     res.status(400).json({ message: message.slice(18) });
   }
