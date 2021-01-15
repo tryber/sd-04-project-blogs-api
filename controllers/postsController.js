90d0095732da34b33286bb082b7186ed6b72fb47
Caio Ribeiro <cacoribeiro@gmail.com>
2021-01-14 21:39:53 -0300

    all good?

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -45,19 +45,34 @@ postsRouter.get('/:id', validateToken, async (req, res) => {
   }
 });
 
-// postsRouter.put('/:id', validateToken, async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const allPosts = await Posts.findOne({
-//       where: { id },
-//       include: [{ model: Users, as: 'user', attributes: { exclude: ['password'] } }],
-//     });
-//     if (!allPosts) return res.status(404).json({ message: 'Post não existe' });
-//     res.status(200).json(allPosts);
-//   } catch (error) {
-//     res.status(404).json({ message: error.message });
-//   }
-// });
+postsRouter.put('/:id', validateToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Posts.findOne({
+      where: { id },
+      include: [{ model: Users, as: 'user', attributes: { exclude: ['password'] } }],
+    });
+    const { title, content } = req.body;
+    if (!title) return res.status(400).json({ message: '"title" is required' });
+    if (!content) return res.status(400).json({ message: '"content" is required' });
+
+    if (req.user.id !== post.userId) {
+      return res.status(401).json({ message: 'Usuário não autorizado' });
+    }
+
+    await Posts.update({ title, content }, { where: { id } });
+
+    const newPost = await Posts.findOne({
+      where: { id },
+      include: [{ model: Users, as: 'user', attributes: { exclude: ['password'] } }],
+    });
+
+    if (!newPost) return res.status(404).json({ message: 'Post não existe' });
+    res.status(200).json(newPost);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
 
 postsRouter.delete('/:id', validateToken, async (req, res) => {
   const { id } = req.params;
