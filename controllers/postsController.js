de7cb5d5a1677a810a4306d693a31525d08bad81
Ronan-Fernandes <ronanf.sk@gmail.com>
2021-01-16 20:21:33 -0300

    requisito 8

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -35,4 +35,27 @@ router.get('/',
       return res.status(500).json({ message: error });
     }
   });
+
+router.get('/:id',
+  validateToken,
+  async (req, res) => {
+    try {
+      const { id } = req.params;
+      const post = await Posts.findOne({
+        where: { id },
+        include: {
+          model: Users,
+          as: 'user',
+          attributes: { exclude: ['password'] },
+        },
+        attributes: { exclude: ['userId'] },
+      });
+
+      if (!post) return res.status(404).json({ message: 'Post não existe' });
+
+      return res.status(200).json(post);
+    } catch (error) {
+      return res.status(500).json({ message: error });
+    }
+  });
 module.exports = router;
