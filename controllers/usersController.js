3deef1115ff51200617839c6ffb86ad8beaa2a1e
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-16 20:14:05 -0300

    Response token when create a user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,13 +1,15 @@
 const userController = require('express').Router();
 const { Users } = require('../models');
 const { validateName, validateEmail, validatePassword } = require('../middlewares/userValidations');
+const { createToken } = require('../authentication');
 
 userController.post('/', validateName, validateEmail, validatePassword, async (req, res) => {
   const { displayName, email, password, image } = req.body;
 
   try {
-    const user = Users.create({ displayName, email, password, image });
-    res.status(201).json({ user });
+    const createdUser = await Users.create({ displayName, email, password, image });
+    const token = createToken(createdUser.dataValues);
+    res.status(201).json({ token });
   } catch (err) {
     console.error(err.message);
     res.status(500).json({ message: 'algo deu errado' });
