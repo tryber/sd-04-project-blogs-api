821161770130f63c2382077cc74d2c4d9b5e937f
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2021-01-16 23:26:54 -0300

    tudo errado

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,4 @@ const loginController = require('./loginController');
 const userController = require('./userController');
 const blogPostController = require('./blogPostController');
 
-
 module.exports = { login: loginController, user: userController, blogPost: blogPostController };
