c988092c438e695b0994ab9d1815b4e36d293ef2
Kaecio <kaecio@pop-os.localdomain>
2021-01-16 23:14:04 -0300

    Iniciando projeto

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
-
+const PORT = 3000;
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.get('/user', (req, res) => {
+  res.send('teste');
+});
+app.listen(PORT, () => console.log(`ouvidno na porta ${PORT}`));
---
+++ migrations/20210117015130-create-BlogPosts.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const PostsTable = queryInterface.createTable('Post', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+    return PostsTable;
+  },
+
+  down: async (queryInterface) => queryInterface.dropTable('Posts'),
+};
