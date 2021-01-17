31a775be6abdf443dabf87d0bd1a99abd8f7eb24
Daniel Pantalena <pantalenadaniel@gmail.com>
2021-01-16 22:09:41 -0300

    Create get posts

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,7 +1,10 @@
 const postsController = require('express').Router();
-const { Posts } = require('../models');
+const { Posts, Users } = require('../models');
 const validatePost = require('../middlewares/postValidation');
 const { validateToken } = require('../authentication');
+const { usersController } = require('.');
+
+const createMessageJSON = (message) => ({ message });
 
 postsController.post('/', validateToken, validatePost, async (req, res) => {
   try {
@@ -14,7 +17,24 @@ postsController.post('/', validateToken, validatePost, async (req, res) => {
     return res.status(201).json(createdPost);
   } catch (err) {
     console.error(err.message);
-    return res.status(500).json('Opss... algo deu errado :/');
+    return res.status(500).json(createMessageJSON('Opss... algo deu errado :/'));
+  }
+});
+
+postsController.get('/', validateToken, async (_req, res) => {
+  try {
+    const posts = await Posts.findAll({
+      include: {
+        model: Users,
+        as: 'user',
+        attributes: { exclude: ['password'] },
+      },
+      attributes: { exclude: ['userId'] },
+    });
+    return res.status(200).json(posts);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json(createMessageJSON('Opss... algo deu errado :/'));
   }
 });
 
