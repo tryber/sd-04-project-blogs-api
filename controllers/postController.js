fa4768438c82f996beb03684b5db27f96edfb974
fogo5000 <lucaslucas2008@gmail.com>
2021-01-17 16:16:00 -0300

    Requisition 9 done

--- controllers/postController.js
+++ controllers/postController.js
@@ -45,4 +45,31 @@ router.get('/:id', jwt.validateJWT, async (req, res) => {
   res.status(200).json(post);
 });
 
+router.put('/:id', jwt.validateJWT, async (req, res) => {
+  const { title, content } = req.body;
+  const token = req.headers.authorization;
+  const decoded = encrypt.verify(token, process.env.SECRET);
+
+  //  title validation
+  if (!title) return res.status(400).json({ message: '"title" is required' });
+
+  //  content validation
+  if (!content) return res.status(400).json({ message: '"content" is required' });
+
+  const { userId } = await Post.findOne({
+    where: { id: req.params.id },
+  });
+
+  //  usuário validation
+  if (decoded.data.id !== userId) {
+    return res.status(401).json({ message: 'Usuário não autorizado' });
+  }
+
+  const editedPost = { title, content, userId: decoded.data.id };
+
+  await Post.update(editedPost, { where: { id: req.params.id } });
+
+  res.status(200).json(editedPost);
+});
+
 module.exports = router;
