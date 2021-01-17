4522e3421e1ee586118e291f606084d29c9f8da8
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2021-01-17 00:19:35 -0300

    ESLint

--- model/index.js
+++ model/index.js
@@ -1,4 +1,5 @@
-'use strict';
+/* eslint-disable global-require */
+// 'use strict';
 
 const fs = require('fs');
 const path = require('path');
@@ -6,7 +7,8 @@ const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+// eslint-disable-next-line import/no-dynamic-require
+const config = require(`${__dirname}/../config/config.js`)[env];
 const db = {};
 
 let sequelize;
@@ -18,15 +20,14 @@ if (config.use_env_variable) {
 
 fs
   .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    // eslint-disable-next-line import/no-dynamic-require
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
