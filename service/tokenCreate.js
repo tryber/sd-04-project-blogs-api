27a61743c3f54fae31463139ffb529a20f1b783e
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2021-01-16 21:00:39 -0300

    Tentando resolver o problema

--- service/tokenCreate.js
+++ service/tokenCreate.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 
+const headers = {
+  expiresIn: '1h', // expira em 1h
+  algorithm: 'HS256',
+};
+
 const secret = 'a1a1a1a1a1a1a1';
 
 const createToken = (payload) => {
-  const headers = {
-    expiresIn: '1h', // expira em 1h
-    algorithm: 'HS256',
-  };
-
   const token = jwt.sign(payload, secret, headers);
 
   return token;
--- service/tokenValidation.js
+++ service/tokenValidation.js
@@ -3,19 +3,18 @@ const jwt = require('jsonwebtoken');
 const secret = 'a1a1a1a1a1a1a1';
 
 const tokenValidation = (req, res, next) => {
-  try {
-    const token = req.headers.authorization;
+  const token = req.headers.authorization;
 
-    if (!token) {
-      return res.status(401).json({
-        message: 'Token não encontrado',
-      });
-    }
+  if (!token) {
+    return res.status(401).json({
+      message: 'Token não encontrado',
+    });
+  }
+
+  try {
     const data = jwt.verify(token, secret);
     const { email } = data;
-
     req.user = { email };
-
     next();
   } catch (err) {
     console.error('validateToken', err.message);
