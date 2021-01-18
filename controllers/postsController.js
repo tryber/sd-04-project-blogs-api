22a4e138e11e476b2ad673d75da28f6f081139f2
matheusysd <matheusyurisd@gmail.com>
2021-01-18 11:50:44 -0300

    Get post by id

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -30,4 +30,26 @@ router.get('/', validateToken, async (_req, res) => {
     return res.status(500).json({ message: error });
   }
 });
+
+router.get('/:id', validateToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Posts.findOne({
+      where: { id },
+      include: {
+        model: Users,
+        as: 'user',
+        attributes: { exclude: ['password'] },
+      },
+      attributes: { exclude: ['userId'] },
+    });
+
+    if (!post) return res.status(404).json({ message: 'Post não existe' });
+
+    return res.status(200).json(post);
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+});
+
 module.exports = router;
