f1ddec82aa9020534185049ccbb6b44a2a3a9d86
matheusysd <matheusyurisd@gmail.com>
2021-01-18 11:10:15 -0300

    Login

---
+++ controllers/loginController.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { createToken } = require('../services/auth');
+const validations = require('../middlewares/loginValidations');
+
+router.post(
+  '/',
+  validations.verifyEmail,
+  validations.verifyPassword,
+  validations.verifyUserExists,
+  async (req, res) => {
+    const { email, password } = req.body;
+
+    const token = createToken({ email, password });
+
+    return res.status(200).json({ token });
+  },
+);
+
+module.exports = router;
