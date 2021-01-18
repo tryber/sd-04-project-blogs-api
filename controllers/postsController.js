85339bb2e2bcada2d04876af9b17a36a336dd32e
matheusysd <matheusyurisd@gmail.com>
2021-01-18 11:49:49 -0300

    Get posts

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { validateToken } = require('../services/auth');
 
-const { Posts } = require('../models');
+const { Posts, Users } = require('../models');
 const verifyFields = require('../middlewares/postsValidation');
 
 router.post('/', validateToken, verifyFields, async (req, res) => {
@@ -12,6 +12,22 @@ router.post('/', validateToken, verifyFields, async (req, res) => {
     return res.status(201).json(newPost);
   } catch (_err) {
     return res.status(500).json({ message: 'algo deu errado' });
+  }
+});
+
+router.get('/', validateToken, async (_req, res) => {
+  try {
+    const posts = await Posts.findAll({
+      include: {
+        model: Users,
+        as: 'user',
+        attributes: { exclude: ['password'] },
+      },
+      attributes: { exclude: ['userId'] },
+    });
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(500).json({ message: error });
   }
 });
 module.exports = router;
