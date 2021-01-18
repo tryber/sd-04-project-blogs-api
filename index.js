2053ad4c6408a57aa10a7b50d8ee627ba7e4713c
Roberto Asgon <robertoasgon@gmail.com>
2021-01-18 13:42:34 -0300

    fix: remove boom lib

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const boom = require('@hapi/boom');
 const { usersController, loginController, postsController } = require('./controllers');
 
 const PORT = 3000;
@@ -11,12 +10,6 @@ app.use('/user', usersController);
 app.use('/post', postsController);
 
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
-
-app.use((err, _req, res, _next) => {
-  if (!boom.isBoom(err)) return res.status(500).send(`${err.message}`);
-
-  res.status(err.output.statusCode).send(`${err.output.payload.message}`);
-});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
